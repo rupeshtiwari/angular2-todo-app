09c5a526e2147340b4bf3bae77068283d91fdf69
roopkt <roopkt@gmail.com>
2017-03-22 18:06:50 -0400

    added all methods in todo-data.service.ts

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Todo } from "app/todo";
 
 @Injectable()
 export class TodoDataService {
 
+  lastId:number =0;
+  toDos:Todo[] =[];
   constructor() { }
 
+  addTodo(todo:Todo):TodoDataService {
+    if(!todo.id)
+    todo.id= ++this.lastId;
+    return this;
+  }
+
+  deleteTodoById(id:number) :TodoDataService {
+    this.toDos = this.toDos.filter(todo=>todo.id!==id);
+    return this;
+  }
+
+  updateTodoById(id:number, values:Object={}):Todo {
+    
+  }
 }
