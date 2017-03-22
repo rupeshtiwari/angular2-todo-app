2a3a5f7e07fc641cd85525d101b8c426d356e6fe
roopkt <roopkt@gmail.com>
2017-03-22 18:07:09 -0400

    added todo-data.service.ts

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -4,22 +4,37 @@ import { Todo } from "app/todo";
 @Injectable()
 export class TodoDataService {
 
-  lastId:number =0;
-  toDos:Todo[] =[];
+  lastId: number = 0;
+  toDos: Todo[] = [];
   constructor() { }
 
-  addTodo(todo:Todo):TodoDataService {
-    if(!todo.id)
-    todo.id= ++this.lastId;
+  addTodo(todo: Todo): TodoDataService {
+    if (!todo.id)
+      todo.id = ++this.lastId;
     return this;
   }
 
-  deleteTodoById(id:number) :TodoDataService {
-    this.toDos = this.toDos.filter(todo=>todo.id!==id);
+  deleteTodoById(id: number): TodoDataService {
+    this.toDos = this.toDos.filter(todo => todo.id !== id);
     return this;
   }
 
-  updateTodoById(id:number, values:Object={}):Todo {
-    
+  getTodoById(id: number) {
+    return this.toDos.filter(todo => todo.id === id).pop();
+  }
+  updateTodoById(id: number, values: Object = {}): Todo {
+    let todo = this.getTodoById(id);
+    if (!todo) return null;
+    Object.assign(todo, values);
+    return todo;
+  }
+  getAllTodos(): Todo[] {
+    return this.toDos;
+  }
+  toggleTodoComplete(todo: Todo): Todo {
+    let updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
   }
 }
