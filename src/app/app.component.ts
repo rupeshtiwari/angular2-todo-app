e7c3e0e50f1e8545eeb47da974f15100f87a3786
Rupesh Tiwari <rupeshtiwari@Rupeshs-Air.home>
2017-03-25 21:47:17 -0400

    added event handler on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,20 @@ import { Todo } from "app/todo";
   providers: [TodoDataService]
 })
 export class AppComponent {
-  newTodo: Todo = new Todo();
   constructor(private todoDataService: TodoDataService) {
   }
- 
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
+  }
+
   toggleTodoComplete(todo) {
     this.todoDataService.toggleTodoComplete(todo);
   }
+
   removeTodo(todo) {
     this.todoDataService.deleteTodoById(todo.id);
   }
+  
   get todos() {
     return this.todoDataService.getAllTodos();
   }
